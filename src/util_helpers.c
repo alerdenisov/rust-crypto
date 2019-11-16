e027e0be8500dcfe29ead59a23e9ca1602d62051
Aler Denisov <aler.zampillo@gmail.com>
2019-11-16 19:17:12 +0300

    fix typo

--- src/util_helpers.c
+++ src/util_helpers.c
@@ -71,7 +71,37 @@ uint32_t rust_crypto_util_fixed_time_eq_asm(uint8_t *lhsp, uint8_t *rhsp,
 }
 #endif
 
-// #ifdef __arm__
+#ifdef __arm__
+uint32_t rust_crypto_util_fixed_time_eq_asm(uint8_t *lhsp, uint8_t *rhsp,
+                                            size_t count) {
+  if (count == 0) {
+    return 1;
+  }
+  uint8_t result = 0;
+  asm(" \
+            1: \
+            \
+            ldrb r4, [%1]; \
+            ldrb r5, [%2]; \
+            eor r4, r4, r5; \
+            orr %0, %0, r4; \
+            \
+            add %1, %1, #1; \
+            add %2, %2, #1; \
+            subs %3, %3, #1; \
+            bne 1b; \
+        "
+      : "+&r"(result), "+&r"(lhsp), "+&r"(rhsp),
+        "+&r"(count)     // all input and output
+      :                  // input
+      : "r4", "r5", "cc" // clobbers
+  );
+
+  return result;
+}
+#endif
+
+// #ifdef __aarch64__
 // uint32_t rust_crypto_util_fixed_time_eq_asm(uint8_t* lhsp, uint8_t* rhsp,
 // size_t count) {
 //     if (count == 0) {
@@ -82,10 +112,10 @@ uint32_t rust_crypto_util_fixed_time_eq_asm(uint8_t *lhsp, uint8_t *rhsp,
 //         " \
 //             1: \
 //             \
-//             ldrb r4, [%1]; \
-//             ldrb r5, [%2]; \
-//             eor r4, r4, r5; \
-//             orr %0, %0, r4; \
+//             ldrb w4, [%1]; \
+//             ldrb w5, [%2]; \
+//             eor w4, w4, w5; \
+//             orr %0, %0, w4; \
 //             \
 //             add %1, %1, #1; \
 //             add %2, %2, #1; \
@@ -93,42 +123,12 @@ uint32_t rust_crypto_util_fixed_time_eq_asm(uint8_t *lhsp, uint8_t *rhsp,
 //             bne 1b; \
 //         "
 //         : "+&r" (result), "+&r" (lhsp), "+&r" (rhsp), "+&r" (count) // all
-//         input and output : // input : "r4", "r5", "cc" // clobbers
+//         input and output : // input : "w4", "w5", "cc" // clobbers
 //     );
 
 //     return result;
 // }
 // #endif
-
-#ifdef __aarch64__
-uint32_t rust_crypto_util_fixed_time_eq_asm(uint8_t *lhsp, uint8_t *rhsp,
-                                            size_t count) {
-  if (count == 0) {
-    return 1;
-  }
-  uint8_t result = 0;
-  asm(" \
-            1: \
-            \
-            ldrb w4, [%1]; \
-            ldrb w5, [%2]; \
-            eor w4, w4, w5; \
-            orr %0, %0, w4; \
-            \
-            add %1, %1, #1; \
-            add %2, %2, #1; \
-            subs %3, %3, #1; \
-            bne 1b; \
-        "
-      : "+&r"(result), "+&r"(lhsp), "+&r"(rhsp),
-        "+&r"(count)     // all input and output
-      :                  // input
-      : "w4", "w5", "cc" // clobbers
-  );
-
-  return result;
-}
-#endif
 
 void rust_crypto_util_secure_memset(uint8_t *dst, uint8_t val, size_t count) {
   memset(dst, val, count);
